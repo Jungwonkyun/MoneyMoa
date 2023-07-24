a279c3f77638ba0527c1b8d67bb21a1e81ff7a39
신경희 <skh1615@gmail.com>
2023-07-24 17:52:12 +0900

    Fix: productStore

--- src/stores/productStore.js
+++ src/stores/productStore.js
@@ -3,9 +3,13 @@ import { defineStore } from 'pinia'
 
 export const useProductStore = defineStore('product', () => {
   const bank = ref('')
-  const amount = ref(0)
-  const period = ref(6)
-  function setBank(bankName) {
-    bank.value = bankName
+  const condition = ref({
+    amount: 0,
+    period: 6
+  })
+  function setCondition(amo, per) {
+    condition.amount = amo
+    condition.period = per
   }
+  return { bank, condition, setCondition }
 })
