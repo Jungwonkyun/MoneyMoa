fac01e8508656163b7777a13e634e0c6da818ea9
신경희 <skh1615@gmail.com>
2023-07-28 16:33:29 +0900

    Fix: api/product.js

--- src/api/product.js
+++ src/api/product.js
@@ -9,4 +9,25 @@ async function getDepositList() {
     console.log(error)
   }
 }
-export { getDepositList }
+async function getSavingList() {
+  try {
+    const response = await api.get('/saving')
+    return response
+  } catch (error) {
+    console.log(error)
+  }
+}
+async function getCMAList() {
+  try {
+    const response = await api.get('/cma')
+    return response
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+//예적금detail리스트 가공을 여기서 해도 될까??
+
+//찜정보post하는 메서드
+
+export { getDepositList, getSavingList, getCMAList }
