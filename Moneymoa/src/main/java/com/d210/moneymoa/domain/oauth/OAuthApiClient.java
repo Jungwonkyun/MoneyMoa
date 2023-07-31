3c80773a2a53f5452f346d6f6755ae3e5672d059
정원균 <wjddnjsrbs97@naver.com>
2023-07-31 16:09:12 +0900

    프론트 branch 정리

--- Moneymoa/src/main/java/com/d210/moneymoa/domain/oauth/OAuthApiClient.java
+++
@@ -1,7 +0,0 @@
-package com.d210.moneymoa.domain.oauth;
-
-public interface OAuthApiClient {
-    OAuthProvider oAuthProvider();
-    String requestAccessToken(OAuthLoginParams params);
-    OAuthInfoResponse requestOauthInfo(String accessToken);
-}
--- Moneymoa/src/main/java/com/d210/moneymoa/domain/oauth/OAuthProvider.java
+++
@@ -1,5 +0,0 @@
-package com.d210.moneymoa.domain.oauth;
-
-public enum OAuthProvider {
-    KAKAO,NAVER,GENERAL
-}
