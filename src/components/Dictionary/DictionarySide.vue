771b2a4163265e9c011cc0a883472120fc93d581
신경희 <skh1615@gmail.com>
2023-07-31 17:51:55 +0900

    Style: dictionaryBtn 위치변경

--- src/components/Dictionary/DictionarySide.vue
+++ src/components/Dictionary/DictionarySide.vue
@@ -1,42 +1,45 @@
 <template>
   <!-- 금융사전 메뉴 -->
-  <div class="text-center">
-    <v-menu v-model="menu" :close-on-content-click="false" location="top end">
-      <template v-slot:activator="{ props }">
-        <v-btn color="indigo" v-bind="props"> <v-icon icon="mdi-magnify"></v-icon> </v-btn>
-      </template>
-      <!-- 여기서 크기조절 -->
-      <v-card width="500" max-height="500">
-        <v-list>
-          <v-list-item title="금융사전">
-            <!-- 금융사전 검색창입니다 -->
-            <v-text-field
-              clearable
-              placeholder="키워드 검색"
-              variant="underlined"
-              append-inner-icon="mdi-magnify"
-            >
-            </v-text-field>
-          </v-list-item>
-        </v-list>
-        <v-divider></v-divider>
+  <v-menu
+    v-model="menu"
+    :close-on-content-click="false"
+    location="start"
+    transition="fab-transition"
+  >
+    <template v-slot:activator="{ props }">
+      <v-btn color="indigo" v-bind="props"> <v-icon icon="mdi-magnify"></v-icon> </v-btn>
+    </template>
+    <!-- 여기서 크기조절 -->
+    <v-card width="500" max-height="500">
+      <v-list>
+        <v-list-item title="금융사전">
+          <!-- 금융사전 검색창입니다 -->
+          <v-text-field
+            clearable
+            placeholder="키워드 검색"
+            variant="underlined"
+            append-inner-icon="mdi-magnify"
+          >
+          </v-text-field>
+        </v-list-item>
+      </v-list>
+      <v-divider></v-divider>
 
-        <v-list>
-          <v-expansion-panels variant="accordion">
-            <DictionaryItem v-for="index in 100" :key="index" :item="list[0].value" />
-          </v-expansion-panels>
-        </v-list>
+      <v-list>
+        <v-expansion-panels variant="accordion">
+          <DictionaryItem v-for="index in 100" :key="index" :item="list[0].value" />
+        </v-expansion-panels>
+      </v-list>
 
-        <!-- 뭐하는앤지 모르겠는데 오류날까봐 일단놔둠 -->
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <!--  -->
+      <!-- 뭐하는앤지 모르겠는데 오류날까봐 일단놔둠 -->
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <!--  -->
 
-          <v-btn variant="text" @click="menu = false"> Cancel </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-menu>
-  </div>
+        <v-btn variant="text" @click="menu = false"> Cancel </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-menu>
 </template>
 
 <script setup>
