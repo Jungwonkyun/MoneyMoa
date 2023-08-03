c600e9bea55941b441c9a6316cd360d40bd88b64
정원균 <wjddnjsrbs97@naver.com>
2023-08-03 16:55:23 +0900

    Update MoneyMoa_BE/src/main/java/com/d210/moneymoa/config/WebConfig.java

--- MoneyMoa_BE/src/main/java/com/d210/moneymoa/config/WebConfig.java
+++ MoneyMoa_BE/src/main/java/com/d210/moneymoa/config/WebConfig.java
@@ -1,31 +1,31 @@
-package com.d210.moneymoa.config;
+// package com.d210.moneymoa.config;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import com.google.gson.Gson;
+// import com.google.gson.GsonBuilder;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.http.converter.HttpMessageConverter;
+// import org.springframework.web.client.RestTemplate;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import javax.annotation.PostConstruct;
-import java.util.List;
+// import javax.annotation.PostConstruct;
+// import java.util.List;
 
-//@EnableWebMvc
+// //@EnableWebMvc
 // @Configuration
 // public class WebConfig implements WebMvcConfigurer {
 
-//     @Override
-//     public void addCorsMappings(CorsRegistry registry) {
-//         registry.addMapping("/**")
-//                 .allowedOrigins("http://localhost:5173","http://127.0.0.1:5173")
-//                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH")
-//                 .allowedHeaders("*")
-//                 .allowCredentials(true)
-//                 .exposedHeaders("Authorization", "RefreshToken");
-//     }
-// }
+//      @Override
+//      public void addCorsMappings(CorsRegistry registry) {
+//          registry.addMapping("/**")
+//                  .allowedOrigins("http://localhost:5173","http://127.0.0.1:5173")
+//                  .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH")
+//                  .allowedHeaders("*")
+//                  .allowCredentials(true)
+//                  .exposedHeaders("Authorization", "RefreshToken");
+//      }
+//  }
 
 
 
