a5d767ad38be40c9b5967e7fe8db996c709c4129
정원균 <wjddnjsrbs97@naver.com>
2023-08-03 15:38:26 +0900

    Update MoneyMoa_BE/src/main/java/com/d210/moneymoa/config/WebConfig.java

--- MoneyMoa_BE/src/main/java/com/d210/moneymoa/config/WebConfig.java
+++ MoneyMoa_BE/src/main/java/com/d210/moneymoa/config/WebConfig.java
@@ -19,7 +19,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173","http://127.0.0.1:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true)
