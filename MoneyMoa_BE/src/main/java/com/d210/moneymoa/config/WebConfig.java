44137f385171a759fbd250973ba88bd0df7a44ad
정원균 <wjddnjsrbs97@naver.com>
2023-08-03 15:55:52 +0900

    Update MoneyMoa_BE/src/main/java/com/d210/moneymoa/config/WebConfig.java, MoneyMoa_BE/src/main/java/com/d210/moneymoa/config/SpringSecurity.java

--- MoneyMoa_BE/src/main/java/com/d210/moneymoa/config/WebConfig.java
+++ MoneyMoa_BE/src/main/java/com/d210/moneymoa/config/WebConfig.java
@@ -13,19 +13,19 @@ import javax.annotation.PostConstruct;
 import java.util.List;
 
 //@EnableWebMvc
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
+// @Configuration
+// public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173","http://127.0.0.1:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .exposedHeaders("Authorization", "RefreshToken");
-    }
-}
+//     @Override
+//     public void addCorsMappings(CorsRegistry registry) {
+//         registry.addMapping("/**")
+//                 .allowedOrigins("http://localhost:5173","http://127.0.0.1:5173")
+//                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH")
+//                 .allowedHeaders("*")
+//                 .allowCredentials(true)
+//                 .exposedHeaders("Authorization", "RefreshToken");
+//     }
+// }
 
 
 
