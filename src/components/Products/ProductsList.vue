a0aac264242c619653b4cd54aa7c06c902f4b720
신경희 <skh1615@gmail.com>
2023-08-04 13:48:57 +0900

    Fix: 예금,적금리스트 v-tab과 router path 양방향바인딩

--- src/components/Products/ProductsList.vue
+++ src/components/Products/ProductsList.vue
@@ -1,44 +1,70 @@
 <template>
-  <div>
-    <BankSelectItem v-if="productType != 'cma'" />
-    <SecuritySelectItem v-else />
-    <ProductConditionItem v-if="productType != 'cma'" />
+  <v-container>
+    <BankSelectItem />
+    <ProductConditionItem />
     <v-divider />
-    결과 {{ filteredProducts.length }} 건
-    <ProductPreviewItem
-      v-for="(product, index) in filteredProducts"
-      :key="index"
-      :product="product"
-      :productType="productType"
-    />
-  </div>
+
+    <v-row v-if="filteredProducts.length == 0">
+      <v-progress-circular indeterminate color="primary"></v-progress-circular>
+    </v-row>
+    <v-row v-else>
+      결과 {{ filteredProducts.length }} 건
+      <ProductPreviewItem
+        v-for="(product, index) in filteredProducts"
+        :key="index"
+        :product="product"
+        :productType="productType"
+      />
+    </v-row>
+  </v-container>
 </template>
 <script setup>
-import { ref, reactive, computed, toRefs } from 'vue'
+import { ref, reactive, toRefs, computed, watch } from 'vue'
 import { useProductStore } from '@/stores/productStore'
 import { storeToRefs } from 'pinia'
+import { useRoute } from 'vue-router'
 import { getDepositList, getSavingList, getPeriodRange, getDeposit, getTest } from '@/api/product'
 import BankSelectItem from './item/BankSelectItem.vue'
-import SecuritySelectItem from './item/SecuritySelectItem.vue'
 import ProductPreviewItem from './item/ProductPreviewItem.vue'
 import ProductConditionItem from './item/ProductConditionItem.vue'
 
 const store = useProductStore()
+const route = useRoute()
 const { productType, amount, period, bankList } = storeToRefs(store)
 const state = reactive({
   products: []
 })
-getDepositList().then((response) => {
-  // console.log(response.data.products)
-  state.products = response.data.products.map((product) => {
-    const bank = bankList.value.find((bank) => bank.alias === product.bankName)
-    if (bank) {
-      return { ...product, bankName: bank.name }
-    } else {
-      return product
+
+watch(
+  productType,
+  (newValue) => {
+    if (newValue === 'saving') {
+      getSavingList().then((response) => {
+        state.products = response.data.products.map((product) => {
+          const bank = bankList.value.find((bank) => bank.alias === product.bankName)
+          if (bank) {
+            return { ...product, bankName: bank.name }
+          } else {
+            return product
+          }
+        })
+      })
+    } else if (newValue === 'deposit') {
+      getDepositList().then((response) => {
+        state.products = response.data.products.map((product) => {
+          const bank = bankList.value.find((bank) => bank.alias === product.bankName)
+          if (bank) {
+            return { ...product, bankName: bank.name }
+          } else {
+            return product
+          }
+        })
+      })
     }
-  })
-})
+  },
+  { immediate: true }
+)
+
 const filteredProducts = computed(() =>
   state.products.filter(
     (product) =>
