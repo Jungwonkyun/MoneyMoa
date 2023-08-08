5b84f4069dd433aacfdd6a43e8f1682239935f71
Noah Kang <no_ahsark@naver.com>
2023-08-08 14:09:39 +0900

    fix: storage download 수정

--- MoneyMoa_BE/src/main/java/com/d210/moneymoa/controller/StorageController.java
+++ MoneyMoa_BE/src/main/java/com/d210/moneymoa/controller/StorageController.java
@@ -3,10 +3,16 @@ package com.d210.moneymoa.controller;
 import com.d210.moneymoa.service.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/file")
@@ -20,14 +26,24 @@ public class StorageController {
     }
 
     @GetMapping("/download/{fileName}")
-    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable String fileName){
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
         byte[] data = storageService.downloadFile(fileName);
         ByteArrayResource resource = new ByteArrayResource(data);
-        return ResponseEntity
-                .ok()
-                .contentLength(data.length)
-                .header("Content-type","application/octet-stream")
-                .header("Content-disposition", "attachment; filename = \"" + fileName + "\" ")
+
+        // Content-Type을 얻기 위해 MimeType 파악
+        String mimeType;
+        try {
+            mimeType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException ex) {
+            mimeType = MediaType.APPLICATION_OCTET_STREAM_VALUE;
+        }
+        if (mimeType == null) {
+            mimeType = MediaType.APPLICATION_OCTET_STREAM_VALUE;
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(mimeType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
                 .body(resource);
     }
 
