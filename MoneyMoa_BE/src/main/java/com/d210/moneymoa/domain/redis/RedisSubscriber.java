688d2a8526b3254692a58dd20f82314f5b7db589
정원균 <wjddnjsrbs97@naver.com>
2023-08-08 20:53:33 +0900

    redis subscriber url 수정

--- MoneyMoa_BE/src/main/java/com/d210/moneymoa/domain/redis/RedisSubscriber.java
+++ MoneyMoa_BE/src/main/java/com/d210/moneymoa/domain/redis/RedisSubscriber.java
@@ -30,7 +30,7 @@ public class RedisSubscriber implements MessageListener {
             // ChatMessage 객채로 맵핑
             ChatMessage roomMessage = objectMapper.readValue(publishMessage, ChatMessage.class);
             // Websocket 구독자에게 채팅 메시지 Send
-            messagingTemplate.convertAndSend("/sub/chat/room/" + roomMessage.getRoomId(), roomMessage);
+            messagingTemplate.convertAndSend("/sub/api/chat/room/" + roomMessage.getRoomId(), roomMessage);
         } catch (Exception e) {
             log.error(e.getMessage());
         }
