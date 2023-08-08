e6f2c73b55794402a69dfc38a852b6abf3d7b2ef
정원균 <wjddnjsrbs97@naver.com>
2023-08-08 13:58:28 +0900

    Update MoneyMoa_BE/src/main/java/com/d210/moneymoa/config/WebSockConfig.java

--- MoneyMoa_BE/src/main/java/com/d210/moneymoa/config/WebSockConfig.java
+++ MoneyMoa_BE/src/main/java/com/d210/moneymoa/config/WebSockConfig.java
@@ -18,7 +18,7 @@ public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/api/ws-stomp")
+        registry.addEndpoint("/api/ws-stomp").setAllowedOriginPatterns("*")
                 .withSockJS();
     }
 }
