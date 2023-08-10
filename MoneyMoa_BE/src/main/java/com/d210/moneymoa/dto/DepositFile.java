3cde603d57472bb41931b99bc60cc654123be341
Noah Kang <no_ahsark@naver.com>
2023-08-10 13:40:13 +0900

    add: DepositFile 생성, Deposit 수정

---
+++ MoneyMoa_BE/src/main/java/com/d210/moneymoa/dto/DepositFile.java
@@ -0,0 +1,32 @@
+package com.d210.moneymoa.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class DepositFile implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String imgPath;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "bank_code", referencedColumnName = "bankCode")
+    private Deposit deposit;
+
+    @Builder
+    public DepositFile(String imgPath, Deposit deposit) {
+        this.imgPath = imgPath;
+        this.deposit = deposit;
+    }
+}
