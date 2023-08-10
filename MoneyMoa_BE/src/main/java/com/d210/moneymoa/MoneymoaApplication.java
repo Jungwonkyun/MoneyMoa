20d7671ecfa32bc5f3b01bf6a6a551c4423c1921
정원균 <wjddnjsrbs97@naver.com>
2023-08-10 15:26:32 +0900

    Update MoneyMoa_BE/src/main/java/com/d210/moneymoa/MoneymoaApplication.java

--- MoneyMoa_BE/src/main/java/com/d210/moneymoa/MoneymoaApplication.java
+++ MoneyMoa_BE/src/main/java/com/d210/moneymoa/MoneymoaApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
+import java.util.TimeZone;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableSwagger2
