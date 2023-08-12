4d442987399b6fe6584f0b3a437cd4835a3f93fd
LeeSangHyuk <makada23@gmail.com>
2023-08-12 18:17:28 +0900

    feedLikeRepository 재추가

---
+++ MoneyMoa_BE/src/main/java/com/d210/moneymoa/repository/FeedLikeRepository.java
@@ -0,0 +1,15 @@
+package com.d210.moneymoa.repository;
+
+
+import com.d210.moneymoa.dto.FeedLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface FeedLikeRepository extends JpaRepository<FeedLike, Long> {
+
+
+    Optional<FeedLike> findByMemberIdAndFeedId(Long memberId,Long feedId);
+}
