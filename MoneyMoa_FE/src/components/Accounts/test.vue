e624ae1c0f2419e27361e8323a7124ce3cb50997
Jonryul <kjr5876@gmail.com>
2023-08-16 16:12:26 +0900

    Feat: 회원정보 수정 사진 업로드시 사진 자르기 기능 추가

---
+++ MoneyMoa_FE/src/components/Accounts/test.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <img id="image" src="picture.jpg" />
+  </div>
+</template>
+<script setup>
+import Cropper from 'cropperjs'
+
+const image = document.getElementById('image')
+const cropper = new Cropper(image, {
+  aspectRatio: 16 / 9,
+  crop(event) {
+    console.log(event.detail.x)
+    console.log(event.detail.y)
+    console.log(event.detail.width)
+    console.log(event.detail.height)
+    console.log(event.detail.rotate)
+    console.log(event.detail.scaleX)
+    console.log(event.detail.scaleY)
+  }
+})
+</script>
+<style lang="scss">
+img {
+  display: block;
+
+  /* This rule is very important, please don't ignore this */
+  max-width: 100%;
+}
+</style>
