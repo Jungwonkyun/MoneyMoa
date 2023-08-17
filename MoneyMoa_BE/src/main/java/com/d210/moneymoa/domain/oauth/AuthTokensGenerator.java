618da430a33017b5d8725eaea922c89185795c9d
Noah Kang <no_ahsark@naver.com>
2023-08-17 13:40:35 +0900

    fix: 토큰 1분으로 변경

--- MoneyMoa_BE/src/main/java/com/d210/moneymoa/domain/oauth/AuthTokensGenerator.java
+++ MoneyMoa_BE/src/main/java/com/d210/moneymoa/domain/oauth/AuthTokensGenerator.java
@@ -19,7 +19,7 @@ import java.util.Date;
 @Getter
 public class AuthTokensGenerator {
     private static final String BEARER_TYPE = "Bearer";
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;  // 30분
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 1;  // 30분
     private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일
 
 
