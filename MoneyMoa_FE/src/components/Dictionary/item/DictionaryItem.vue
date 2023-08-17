c79baeb911ec8c0d0ba3129080b5195d51368486
신경희 <skh1615@gmail.com>
2023-08-17 23:24:56 +0900

    Style: 금융사전 FAB제거, 검색일치 하이라이트 적용

--- MoneyMoa_FE/src/components/Dictionary/item/DictionaryItem.vue
+++ MoneyMoa_FE/src/components/Dictionary/item/DictionaryItem.vue
@@ -1,37 +1,61 @@
 <template>
   <v-expansion-panel>
-    <v-expansion-panel-title
-      ><span v-html="highlightMatchedText(item.term)"></span>
+    <v-expansion-panel-title>
+      <span class="highlighted-value">
+        <template v-for="(part, index) in highlightMatchedText(item.term)" :key="index">
+          <strong v-if="part.matched" class="matched-text">{{ part.text }}</strong>
+          <span v-else>{{ part.text }}</span>
+        </template>
+      </span>
     </v-expansion-panel-title>
     <v-expansion-panel-text>
-      <span v-html="highlightMatchedText(item.definition)"></span>
+      <span>
+        <template v-for="(part, index) in highlightMatchedText(item.definition)" :key="index">
+          <strong v-if="part.matched" class="matched-text">{{ part.text }}</strong>
+          <span v-else>{{ part.text }}</span>
+        </template>
+      </span>
     </v-expansion-panel-text>
   </v-expansion-panel>
 </template>
 
 <script setup>
-import { defineProps, onMounted } from 'vue'
+import { defineProps } from 'vue'
 
 const props = defineProps({
   item: Object,
   searchWord: String
 })
 
-// 검색어와 일치하는 부분을 진하게 표시하는 함수
+// 검색어와 일치하는 부분을 강조 표시하는 함수
 function highlightMatchedText(text) {
   if (text === undefined) {
-    return text
+    return []
   }
   if (props.searchWord) {
     const regex = new RegExp(props.searchWord, 'gi')
-    return text.replace(regex, (matched) => `<strong>${matched}</strong>`)
+    const parts = []
+    let lastIndex = 0
+    let match
+    while ((match = regex.exec(text)) !== null) {
+      if (match.index > lastIndex) {
+        parts.push({ text: text.slice(lastIndex, match.index), matched: false })
+      }
+      parts.push({ text: match[0], matched: true })
+      lastIndex = regex.lastIndex
+    }
+    if (lastIndex < text.length) {
+      parts.push({ text: text.slice(lastIndex), matched: false })
+    }
+    return parts
   } else {
-    return text
+    return [{ text, matched: false }]
   }
 }
-onMounted(() => {
-  highlightMatchedText()
-})
 </script>
 
-<style></style>
+<style scoped lang="scss">
+.matched-text {
+  background-color: rgb(209, 237, 255);
+}
+</style>
